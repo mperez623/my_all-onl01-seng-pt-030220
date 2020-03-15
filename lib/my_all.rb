b31a9b04ae3e1a3389fdaca3c1b308afa221ce6b
Melissa Perez <melissaperezg.96@gmail.com>
2020-03-15 18:24:06 +0000

    Done.

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -1,5 +1,13 @@
 require 'pry'
 
 def my_all?(collection)
-
+  i = 0
+  while i < collection.length 
+    bouncer = yield(collection[i])
+    if !bouncer
+      return false
+    end 
+    i += 1
+  end
+return true
 end
